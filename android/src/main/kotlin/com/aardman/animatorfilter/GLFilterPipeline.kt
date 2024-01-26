41fee541fccc0de40234dcffe7e04ee062014b74
Paul Freeman <paul@paulfreeman.com>
2024-01-26 12:08:40 +0000

    YUV textures populated prior to conversion shader call

--- android/src/main/kotlin/com/aardman/animatorfilter/GLFilterPipeline.kt
+++ android/src/main/kotlin/com/aardman/animatorfilter/GLFilterPipeline.kt
@@ -1,14 +1,20 @@
 package com.aardman.animatorfilter
 
 import android.graphics.Bitmap
+import android.icu.text.DateTimePatternGenerator.PatternInfo.OK
 import android.opengl.EGL14
 import android.opengl.EGLConfig
 import android.opengl.EGLExt
 import android.opengl.GLES30
+import android.opengl.GLES30.GL_RED
+import android.opengl.GLES30.GL_UNSIGNED_BYTE
+import android.renderscript.Double4
 import android.util.Size
 import android.view.Surface
 import com.aardman.animatorfilter.GLUtils.checkEglError
-import com.aardman.animatorfilter.GLUtils.getBitmapFromTexture
+import com.aardman.animatorfilter.GLUtils.checkTexturePixels
+import com.aardman.animatorfilter.GLUtils.padArrays
+import com.aardman.animatorfilter.GLUtils.padByteArray
 import com.aardman.animatorfilter.GLUtils.setupFramebuffer
 import com.aardman.animatorfilter.GLUtils.setupShaderProgram
 import com.aardman.animatorfilter.GLUtils.setupVertexArrayForProgram
@@ -81,6 +87,7 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 		eglSetup()
 		makeCurrent()
 		setupOpenGLObjects()
+		makeCurrent()  //only one context set it up at the start
 	}
 
 	private fun eglSetup() {
@@ -143,14 +150,24 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 		setupTestQuadShader()
 	}
 
+	/**
+	 * NB: The input UV planes are a byte short of the Y plane size / 2 out of the Android Camera
+	 * Thus the last byte in each U V plane is missing
+	 */
 	private fun setupTextures(){
 		// Create the texture that will hold the source image
 		workingTexture1 = GLUtils.createTexture(textureWidth, textureHeight)
 		workingTexture2 = GLUtils.createTexture(textureWidth, textureHeight)
 		backgroundImageTexture = GLUtils.createTexture(textureWidth, textureHeight)
-		srcYTexture    = GLUtils.createTexture(textureWidth, textureHeight)
-		srcUTexture    = GLUtils.createTexture(textureWidth/2, textureHeight/2)
-		srcVTexture    = GLUtils.createTexture(textureWidth/2, textureHeight/2)
+
+		//Monochrome textures, this uses 8 bit signed raw data from a byte array but will be interpreted correctly despite being signed from a ByteArray
+		srcYTexture    = GLUtils.createTexture(textureWidth, textureHeight, GLES30.GL_R8, GL_RED, GL_UNSIGNED_BYTE )
+
+		//TODO: It is incorrect to / dimensions by 2 as the textures will be too small  to accomodate the required data
+		//Instead make the U and  V textures  the  same  size  as the  Y texture, then fill a subset of the texture based on
+		//the aspect ratio of  the input
+		srcUTexture    = GLUtils.createTexture(textureWidth , textureHeight, GLES30.GL_R8, GL_RED, GL_UNSIGNED_BYTE )
+		srcVTexture    = GLUtils.createTexture(textureWidth, textureHeight, GLES30.GL_R8, GL_RED, GL_UNSIGNED_BYTE )
 	}
 
 	//Set up the framebuffer that will be used to render the results
@@ -179,35 +196,6 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 		GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, texCoordsBuffer.capacity() * 4, texCoordsBuffer, GLES30.GL_STATIC_DRAW)
 
 		texVBO = texCoordBuffer[0]
-	}
-
-	private fun setupConverter() {
-		this.yuvConversionProgram = setupShaderProgram(VertexShaderSource, conversionShader)
-
-		// Get vertex shader attributes
-		this.attributes["c_texCoord"] = GLES30.glGetAttribLocation(this.yuvConversionProgram, "a_texCoord")
-
-		// ... other specific setups like uniforms
-		// Find uniforms
-		this.uniforms["yTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "yTexture")
-		this.uniforms["vTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "vTexture")
-		this.uniforms["uTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "uTexture")
-
-		//Enable related attributes (might be in a more generic location, but this sequence is required
-		GLES30.glEnableVertexAttribArray(this.attributes["c_texCoord"]!!)
-		checkEglError("enableVertexAttribArray")
-		// Describe how to pull data out of the buffer, take 2 items per iteration (x and y)
-		GLES30.glVertexAttribPointer(this.attributes["c_texCoord"]!!, 2, GLES30.GL_FLOAT, false, 0, 0)
-		checkEglError("glVertexAttribPointer")
-
-	    yuvConversionVAO = GLUtils.setupVertexArrayForProgram(yuvConversionProgram)
-		checkEglError("generate vertex arrays")
-		GLES30.glBindVertexArray(yuvConversionVAO)
-
-		//Enable related attributes, link with currently bound VAO
-		GLES30.glEnableVertexAttribArray(this.attributes["c_texCoord"]!!)
-		// Describe how to pull data out of the buffer, take 2 items per iteration (x and y)
-		GLES30.glVertexAttribPointer(this.attributes["c_texCoord"]!!, 2, GLES30.GL_FLOAT, false, 0, 0)
 	}
 
 	//Setup the chroma filter
@@ -299,27 +287,76 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 
 	}
 
+	private fun setupConverter() {
+		this.yuvConversionProgram = setupShaderProgram(VertexShaderSource, conversionShader)
+
+		// Get vertex shader attributes
+		this.attributes["c_texCoord"] = GLES30.glGetAttribLocation(this.yuvConversionProgram, "a_texCoord")
+
+		// ... other specific setups like uniforms
+		// Find uniforms
+		this.uniforms["yTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "yTexture")
+		this.uniforms["vTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "vTexture")
+		this.uniforms["uTexture"] = GLES30.glGetUniformLocation(this.yuvConversionProgram, "uTexture")
+
+		//Enable related attributes (might be in a more generic location, but this sequence is required
+		GLES30.glEnableVertexAttribArray(this.attributes["c_texCoord"]!!)
+		checkEglError("enableVertexAttribArray")
+		// Describe how to pull data out of the buffer, take 2 items per iteration (x and y)
+		GLES30.glVertexAttribPointer(this.attributes["c_texCoord"]!!, 2, GLES30.GL_FLOAT, false, 0, 0)
+		checkEglError("glVertexAttribPointer")
+
+		yuvConversionVAO = GLUtils.setupVertexArrayForProgram(yuvConversionProgram)
+		checkEglError("generate vertex arrays")
+		GLES30.glBindVertexArray(yuvConversionVAO)
+
+		//Enable related attributes, link with currently bound VAO
+		GLES30.glEnableVertexAttribArray(this.attributes["c_texCoord"]!!)
+		// Describe how to pull data out of the buffer, take 2 items per iteration (x and y)
+		GLES30.glVertexAttribPointer(this.attributes["c_texCoord"]!!, 2, GLES30.GL_FLOAT, false, 0, 0)
+	}
+
 	//Converts the srcYUV textures to RGB and stores in workingTexture
 	private fun convertYUV(width: Int, height: Int) {
 
 		// Bind the framebuffer where workingTexture is enabled
 		GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, workingFBO1)
 
-		//Use conversion program and set parameters
 		GLES30.glUseProgram(this.yuvConversionProgram)
-		GLES30.glUniform1i(this.uniforms["yTexture"]!!, this.srcYTexture)
-		GLES30.glUniform1i(this.uniforms["uTexture"]!!, this.srcUTexture)
-		GLES30.glUniform1i(this.uniforms["vTexture"]!!, this.srcVTexture)
+
+		// Activate and bind the Y texture
+		GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
+		GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, this.srcYTexture)
+		GLES30.glUniform1i(this.uniforms["yTexture"]!!, 0) // Texture unit 0
+
+		// Activate and bind the U texture
+		GLES30.glActiveTexture(GLES30.GL_TEXTURE1)
+		GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, this.srcUTexture)
+		GLES30.glUniform1i(this.uniforms["uTexture"]!!, 1) // Texture unit 1
+
+		// Activate and bind the V texture
+		GLES30.glActiveTexture(GLES30.GL_TEXTURE2)
+		GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, this.srcVTexture)
+		GLES30.glUniform1i(this.uniforms["vTexture"]!!, 2) // Texture unit 2
 
 		//Set the viewport
 		GLES30.glViewport(0, 0, width, height)
 		GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT)
+
+		//Enable related attributes, link with currently bound VAO
+		GLES30.glEnableVertexAttribArray(this.attributes["c_texCoord"]!!)
+
+		//Bind the VBO
+		GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, texVBO)
 
 		//Bind the VAO
 		GLES30.glBindVertexArray(yuvConversionVAO)
 
 		//Draw to the currently bound texture using the program
 		GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 6)
+
+		val texturePopulated = checkTexturePixels(workingTexture1, textureWidth, textureHeight, GLES30.GL_RGBA,null)
+		print ("converted workingTexture1 contains pixels = " + texturePopulated)
 
 		//Unbind vao
 		GLES30.glBindVertexArray(0)
@@ -479,7 +516,6 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 	//TODO: delete after pipeline tested
 	//Gaussian example applied to background
 	fun drawWithFilter(radius: Float, flip: Boolean = false) {
-		makeCurrent()
 
 		// Tell it to use our program
 		GLES30.glUseProgram(this.filterProgram)
@@ -509,21 +545,30 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 		GLUtils.checkEglError("eglSwapBuffers")
 	}
 
+
 	//Main draw method for chromakey processing
 	fun draw (yBytes: ByteArray, uBytes: ByteArray, vBytes: ByteArray, width: Int, height: Int, radius: Float, flip: Boolean = false) {	// *** A ****
 
 		// *** A ****
-		//Load Y U V data into existing textures to use in image conversion
-		GLUtils.updateTextures(yBytes,srcYTexture, uBytes, srcUTexture, vBytes, srcVTexture, width, height)
+		//Load Y U V data ( into existing textures to use in image conversion
 
-		var resultY = getBitmapFromTexture(srcYTexture, textureWidth, textureHeight)
-		var resultU = getBitmapFromTexture(srcUTexture, textureWidth / 2, textureHeight /  2)
-		var resultV = getBitmapFromTexture(srcVTexture, textureWidth / 2, textureHeight /  2)
+		//Pad U V if necessary
+		val (uBytesPadded, vBytesPadded) = padArrays(yBytes, uBytes, vBytes)
+
+		GLUtils.updateYUVTextures(yBytes, srcYTexture, uBytesPadded, srcUTexture, vBytesPadded, srcVTexture, width, height)
+
+		//TODO: delete these as they are just for debugging by viewing individual bitmaps in debugger
+	    var resultY = checkTexturePixels(srcYTexture, textureWidth, textureHeight, GLES30.GL_RED, yBytes)
+ 		var resultU = checkTexturePixels(srcUTexture, textureWidth/2, textureHeight/2,   GLES30.GL_RED, uBytesPadded)
+ 		var resultV = checkTexturePixels(srcVTexture, textureWidth/2, textureHeight/2,   GLES30.GL_RED, vBytesPadded)
+ 		//print( resultY ?: "Y OK" + resultU  ?: "U OK" + resultV ?: "V OK")
 
 		// *** B ****
 		convertYUV(width, height)
 		GLUtils.checkEglError("convertYUV")
 		//The texture workingTexture now contains the results of the conversion
+		val (bytes, buffer) = checkTexturePixels(workingTexture1, textureWidth, textureHeight, GLES30.GL_RGBA, null)
+		print ("converted workingTexture1 sampled pixels from = " + bytes + " to "+ buffer)
 
 		// *** C ****
 		applyFilter()
@@ -548,9 +593,9 @@ class GLFilterPipeline(private val outSurface: Surface, private val textureWidth
 	//The main function executed on each camera frame
 	public fun render(yBytes: ByteArray, uBytes: ByteArray, vBytes: ByteArray, width: Int, height: Int, radius: Float, flip: Boolean = false) {
      	makeCurrent()
-     	//draw(yBytes,uBytes,vBytes, width, height, radius, flip)  ///FINAL functionality
+     	draw(yBytes,uBytes,vBytes, width, height, radius, flip)
 	    //drawWithFilter(1f, true)  //Test coherence with simple gaussian sample
-		drawTestQuad()  //draw red quad on screen
+		//drawTestQuad()  //draw red quad on screen
 	}
 
 	public fun updateParameters(filterParameters: FilterParameters){
--- android/src/main/kotlin/com/aardman/animatorfilter/GLUtils.kt
+++ android/src/main/kotlin/com/aardman/animatorfilter/GLUtils.kt
@@ -4,10 +4,8 @@
 	import android.opengl.EGL14
 	import android.opengl.GLES30
 	import android.util.Log
-	import java.nio.Buffer
 	import java.nio.ByteBuffer
 	import java.nio.ByteOrder
-	import kotlin.random.Random
 
 
 	/**
@@ -77,46 +75,49 @@
 			return newFrameBuffer
 		}
 
+		//TODO: check
+		fun packYUV(inputArray: ByteArray, rowStride: Int, pixelStride: Int): ByteArray {
+			val outputArray = ByteArray(inputArray.size / pixelStride)
+			var outputIndex = 0
+
+			for (i in inputArray.indices step rowStride) {
+				for (j in i until i + rowStride step pixelStride) {
+					outputArray[outputIndex++] = inputArray[j]
+				}
+			}
+
+			return outputArray
+		}
+
 		fun updateTextureFromPlane(textureId: Int, planeData: ByteArray, width: Int, height: Int) {
-			// Bind to the existing texture in OpenGL
+
+			// Bind to the existing texture in OpenGL, using default texture unit 0
+			// as we do not need more than one texture unit at a time
 			GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textureId)
 			checkEglError("bindTexture")
 
-			// Update the texture with new plane data
-			GLES30.glTexSubImage2D(GLES30.GL_TEXTURE_2D, 0, 0, 0, width, height, GLES30.GL_LUMINANCE, GLES30.GL_UNSIGNED_BYTE, ByteBuffer.wrap(planeData))
+			// Update the texture with new planar data
+			GLES30.glTexSubImage2D(GLES30.GL_TEXTURE_2D, 0, 0, 0, width, height, GLES30.GL_RED, GLES30.GL_UNSIGNED_BYTE, ByteBuffer.wrap(planeData))
 			checkEglError("load texture")
 		}
 
-		fun updateTextures(yPlane: ByteArray, yTextureId: Int,
-						   uPlane: ByteArray, uTextureId: Int,
-						   vPlane: ByteArray, vTextureId: Int,
-						   width: Int, height: Int)  {
+		/*
+		 * Android camera image U, V, planes are sampled with alternate pixels, hence the need to pack the data into a 1/2 size array before filling the textures.
+		 */
+		fun updateYUVTextures(yPlane: ByteArray, yTextureId: Int,
+							  uPlane: ByteArray, uTextureId: Int,
+							  vPlane: ByteArray, vTextureId: Int,
+							  width: Int, height: Int)  {
+
+			val sampledUPlane = packYUV(uPlane, width, 2)
+			val sampledVPlane = packYUV(vPlane, width, 2)
+
 			val yTexture = updateTextureFromPlane(yTextureId, yPlane, width, height)
-			val uTexture = updateTextureFromPlane(uTextureId,uPlane, width / 2, height / 2) // Assuming chroma planes are half the size of luma for YUV420_888 in Android
-			val vTexture = updateTextureFromPlane(vTextureId,vPlane, width / 2, height / 2)
+			val uTexture = updateTextureFromPlane(uTextureId, sampledUPlane, width / 2, height / 2) // Assuming chroma planes are half the size of luma for YUV420_888 in Android
+			val vTexture = updateTextureFromPlane(vTextureId, sampledVPlane, width / 2, height / 2)
 		}
 
 		fun createTexture(width: Int, height: Int, internalFormat: Int = GLES30.GL_RGBA, format: Int = GLES30.GL_RGBA, type: Int = GLES30.GL_UNSIGNED_BYTE): Int {
-			val texture = IntArray(1)
-			GLES30.glGenTextures(1, texture, 0)
-			GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, texture[0])
-
-			GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_CLAMP_TO_EDGE)
-			GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_CLAMP_TO_EDGE)
-			GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_NEAREST)
-			GLES30.glTexParameteri(GLES30.GL_TEXTURE_2D, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_NEAREST)
-
-			// Initialize the texture with no data.
-			val mipLevel = 0 // the largest mip
-			val border = 0
-			GLES30.glTexImage2D(GLES30.GL_TEXTURE_2D, mipLevel, internalFormat, width, height, border, format, type, null)
-
-			GLES30.glGetError() // Check for OpenGL errors.
-
-			return texture[0]
-		}
-
-		fun createMonochromeTexture(width: Int, height: Int, internalFormat: Int = GLES30.GL_RED, format: Int = GLES30.GL_RED, type: Int = GLES30.GL_UNSIGNED_BYTE): Int {
 			val texture = IntArray(1)
 			GLES30.glGenTextures(1, texture, 0)
 			GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, texture[0])
@@ -171,7 +172,7 @@
 			}
 			//debug all openGL calls
 	//		else {
-	//			print(msg + "\n")
+	//			print("EGL " + msg + "\n")
 	//		}
 		}
 
@@ -205,38 +206,6 @@
 			return vao[0]
 		}
 
-		//Debug methods
-		fun getBitmapFromTexture(textureId: Int, textureWidth: Int, textureHeight: Int): Bitmap {
-			// Create a framebuffer and attach the texture to it
-			val frameBuffer = IntArray(1)
-			GLES30.glGenFramebuffers(1, frameBuffer, 0)
-			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, frameBuffer[0])
-			GLES30.glFramebufferTexture2D(GLES30.GL_FRAMEBUFFER, GLES30.GL_COLOR_ATTACHMENT0, GLES30.GL_TEXTURE_2D, textureId, 0)
-
-			// Check for framebuffer completeness
-			if (GLES30.glCheckFramebufferStatus(GLES30.GL_FRAMEBUFFER) != GLES30.GL_FRAMEBUFFER_COMPLETE) {
-				throw RuntimeException("Framebuffer not complete")
-			}
-
-			// Prepare a buffer to store the pixels
-			val buffer = ByteBuffer.allocateDirect(textureWidth * textureHeight * 4)
-			buffer.order(ByteOrder.nativeOrder())
-
-			// Read pixels from the framebuffer into the buffer
-			GLES30.glReadPixels(0, 0, textureWidth, textureHeight, GLES30.GL_RGBA, GLES30.GL_UNSIGNED_BYTE, buffer)
-
-			// Create a bitmap from the buffer
-			val bitmap = Bitmap.createBitmap(textureWidth, textureHeight, Bitmap.Config.ARGB_8888)
-			buffer.rewind() // Rewind the buffer to read from the beginning
-			bitmap.copyPixelsFromBuffer(buffer)
-
-			// Clean up: Unbind and delete the framebuffer returning to the calling state
-			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, 0)
-			GLES30.glDeleteFramebuffers(1, frameBuffer, 0)
-
-			return bitmap
-		}
-
 		fun getBitmapFromFBO(textureWidth: Int, textureHeight: Int, workingFBO: Int): Bitmap {
 			// Bind the framebuffer to which the texture is attached
 			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, workingFBO)
@@ -257,6 +226,91 @@
 			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, 0)
 
 			return bitmap
+		}
+
+		//Debug methods
+		fun checkTexturePixels(textureId: Int, textureWidth: Int, textureHeight: Int, format:Int = GLES30.GL_RGBA, bytesToCheck:ByteArray?):Pair<ByteArray, ByteArray>  {
+			// Create a framebuffer and attach the texture to it
+			val frameBuffer = IntArray(1)
+			GLES30.glGenFramebuffers(1, frameBuffer, 0)
+			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, frameBuffer[0])
+			GLES30.glFramebufferTexture2D(GLES30.GL_FRAMEBUFFER, GLES30.GL_COLOR_ATTACHMENT0, GLES30.GL_TEXTURE_2D, textureId, 0)
+
+			// Check for framebuffer completeness
+			if (GLES30.glCheckFramebufferStatus(GLES30.GL_FRAMEBUFFER) != GLES30.GL_FRAMEBUFFER_COMPLETE) {
+				throw RuntimeException("Framebuffer not complete")
+			}
+
+			// Prepare a buffer to store the pixels
+			val buffer = ByteBuffer.allocateDirect(textureWidth * textureHeight)
+			buffer.order(ByteOrder.nativeOrder())
+
+			// Read pixels from the framebuffer into the buffer
+			GLES30.glReadPixels(0, 0, textureWidth, textureHeight, format, GLES30.GL_UNSIGNED_BYTE, buffer)
+
+			GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, 0)
+			GLES30.glDeleteFramebuffers(1, frameBuffer, 0)
+
+			if(bytesToCheck == null) {
+				val res = containsNonZeroValue(buffer)
+				val outputArray = ByteArray(buffer.remaining())
+				buffer.get(outputArray)
+				return Pair(ByteArray(0),  outputArray)
+			}
+			else {
+				val outputArray = ByteArray(buffer.remaining())
+				buffer.get(outputArray)
+				return Pair(bytesToCheck, outputArray)
+			}
+		}
+
+		fun containsNonZeroValue(buffer: ByteBuffer): Boolean {
+			while (buffer.hasRemaining()) {
+				if (buffer.get().toInt() != 0) {
+					return true
+				}
+			}
+			return false
+		}
+
+		fun returnBufferSamples(byteArray: ByteArray, buffer: ByteBuffer): Pair<ByteArray, ByteArray> {
+			val minLength = minOf(byteArray.size, buffer.remaining(), 50)
+			val first50BytesArray = byteArray.copyOfRange(0, minLength)
+			val first50BytesBuffer = ByteArray(minLength)
+			buffer.rewind()
+			buffer.get(first50BytesBuffer, 0, minLength)
+			return Pair(first50BytesArray, first50BytesBuffer)
+		}
+
+		//Generic  helper functions
+		public fun padByteArray(b1: ByteArray, padding: Int): ByteArray {
+			val paddedArray = ByteArray(b1.size + padding)
+			System.arraycopy(b1, 0, paddedArray, 0, b1.size)
+			// The remaining bytes in paddedArray are automatically initialized to zero
+			return paddedArray
+		}
+
+		/**
+		 * Pad byte u and v byte arrays to 1/2 yByte array length if required padding
+		 * with extra zero values if needed.
+		 */
+		public fun padArrays(yBytes: ByteArray, uBytes: ByteArray, vBytes: ByteArray): Pair<ByteArray, ByteArray>
+		{
+			val requiredUVSize = yBytes.size / 2
+			var paddedUBytes: ByteArray? = null
+			var paddedVBytes: ByteArray? = null
+
+			if (uBytes.size < requiredUVSize) {
+				paddedUBytes = padByteArray(uBytes, requiredUVSize - uBytes.size )
+			}
+			if (vBytes.size < requiredUVSize) {
+				paddedVBytes = padByteArray(vBytes, requiredUVSize - vBytes.size )
+			}
+
+			val retUByte = if (paddedUBytes == null) uBytes else paddedUBytes
+			val retVByte = if (paddedVBytes == null) vBytes else paddedVBytes
+
+			return Pair(retUByte, retVByte)
 		}
 
 
