18ac8ffed33be06c6f252527ce5e4f6628d619b4
Paul Freeman <paul@paulfreeman.com>
2024-01-30 15:25:27 +0000

    Complete API on iOS plugin side

--- ios/Classes/AnimatorfilterPlugin.swift
+++ ios/Classes/AnimatorfilterPlugin.swift
@@ -13,22 +13,27 @@ public class AnimatorfilterPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
     case "create":
         controller.initialize()
       result("iOS  initialised");
     case "initialise":
         controller.initialize()
       result("iOS  initialised");
+    case "setBackroundImagePath":
+        controller.initialize()
+        result("iOS  setBackroundImagePath");
     case "update":
-      result("iOS  updated");
-    case "updateFilters":
-      result("iOS  updateFilters");
+      result("iOS  update");
+    case "updateParameters":
+      result("iOS  updateParameters");
     case "enableFilters":
       result("iOS  enableFilters");
     case "disableFilters":
       result("iOS  disableFilters");
+    case "dispose":
+      result("iOS " + UIDevice.current.systemVersion)
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
     default:
       result(FlutterMethodNotImplemented)
     }
